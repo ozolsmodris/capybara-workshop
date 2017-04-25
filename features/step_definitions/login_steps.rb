a382d7b5f60c7f156e690ad66a5ad1c8eddda2dd
ubn <modris.ozols@testdevlab.com>
2017-04-25 11:43:31 +0300

    if added

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,25 +1,32 @@
 When(/^I am on Appimation login page/) do
   visit('/')
-  find(:button, 'start_button').visible?
-  puts "Try now button found"
+  if find(:button, 'start_button').visible?
+    puts "Try now button found"
+  end
 
-  find(:button, 'signup-b').visible?
-  puts "Signup button found"
+  if find(:button, 'signup-b').visible?
+    puts "Signup button found"
+  end
 
-  find(:xpath, '//h2[contains(text(), "Contact Us")]/parent::section').visible?
-  puts 'Contact Us found'
+  if find(:xpath, '//h2[contains(text(), "Contact Us")]/parent::section').visible?
+    puts 'Contact Us found'
+  end
 
-  find(:xpath, '//*[@id="name"]').visible?
-  puts "Name field found"
+  if find(:xpath, '//*[@id="name"]').visible?
+    puts "Name field found"
+  end
 
-  find(:xpath, '//*[@id="email"]').visible?
-  puts "Email field found"
+  if find(:xpath, '//*[@id="email"]').visible?
+    puts "Email field found"
+  end
 
-  find(:xpath, '//*[@id="contactus-message"]').visible?
-  puts "Message field found"
+  if find(:xpath, '//*[@id="contactus-message"]').visible?
+    puts "Message field found"
+  end
 
-  find(:xpath, '//*[@id="contactus-button"]').visible?
-  puts "Send button found"
+  if find(:xpath, '//*[@id="contactus-button"]').visible?
+    puts "Send button found"
+  end
 
   all(:xpath, '//section[@class="box special features"]/descendant::section').each do |elem|
     puts elem
