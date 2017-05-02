b07fef40d9eb03e7194e352c618e4d0ae9e343bc
ubn <modris.ozols@testdevlab.com>
2017-05-02 13:08:11 +0300

    Revert "lekcija 3"
    This reverts commit efbad34b08bbd969b4a20feb17916bf6e655e2d4.

--- features/demo.feature
+++ features/demo.feature
@@ -3,7 +3,3 @@ Feature: Demo Feature
     Scenario: Open SignUp Page
         Given I am on Appimation home page
         When I click Try Now
-        And I enter example@example.com in SignUp email
-        And I enter Parole123 in SignUp passwords
-        And I enter BestProject in SignUp project name
-        And I cancel SignUp
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,26 +1,23 @@
 When(/^I am on Appimation home page/) do
   visit('/')
-  find(:id, 'start_button').visible?
-  find(:id, 'login-b').visible?
-  find(:id, 'signup-b').visible?
+  
 end
 
 Then(/^I click Try Now/) do
-  find(:id, 'start_button').click
+  
 end
-Then(/^I enter (.*) in SignUp email/) do |email|
-  find(:xpath, '//div[@id="signup"]/descendant::input[@name = "login"]').send_keys email
-end
-
-Then(/^I enter (.*) in SignUp passwords/) do |password|
-  find(:xpath, '//div[@id="signup"]/descendant::input[@name = "password1"]').send_keys password
-  find(:xpath, '//div[@id="signup"]/descendant::input[@name = "password2"]').send_keys password
+Then(/^I enter (.*) in SignUp email/) do
+  
 end
 
-Then(/^I enter (.*) in SignUp project name/) do |project_name|
-  find(:xpath, '//div[@id="signup"]/descendant::input[@name = "project_name"]').send_keys project_name
+Then(/^I enter (.*) in SignUp passwords/) do
+  
+end
+
+Then(/^I enter (.*) in SignUp project name/) do
+  
 end
 
 Then(/^I cancel SignUp/) do
-  find(:xpath, '//div[@id="signup"]/descendant::img[@class = "closecross"]').click
+  
 end
